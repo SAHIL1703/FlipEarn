95ebd7573737e4ab2342cae69598f6ea9ea8ae02
sahilpisal220 <sahilpisal1703@gmail.com>
2025-12-25 13:36:46 +0530

    Added the React Basic setup

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import MarketPlace from "./pages/MarketPlace.jsx";
+import MyListings from "./pages/MyListings.jsx";
+import ListingDetails from "./pages/ListingDetails.jsx";
+import ManageListing from "./pages/ManageListing.jsx";
+import Messages from "./pages/Messages.jsx";
+import MyOrders from "./pages/MyOrders.jsx";
+import Loading from "./pages/Loading.jsx";
+import Navbar from "./components/Navbar.jsx";
+
+const App = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <div>
+      {!pathname.includes("/admin") && <Navbar />}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/marketplace" element={<MarketPlace />} />
+        <Route path="/my-listings" element={<MyListings />} />
+        <Route path="/listing/:listingId" element={<ListingDetails />} />
+        <Route path="/create-listing" element={<ManageListing />} />
+        <Route path="/edit-listing/:id" element={<ManageListing />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/my-orders" element={<MyOrders />} />
+        <Route path="/loading" element={<Loading />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/pages/MyListings.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyListings = () => {
+  return (
+    <div><h1>MyListings</h1></div>
+  )
+}
+
+export default MyListings
