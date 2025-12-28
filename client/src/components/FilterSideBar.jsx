7d9df5f7e4e5cc15b010341e2142ea503378efc7
sahilpisal220 <sahilpisal1703@gmail.com>
2025-12-28 14:31:11 +0530

    Working on the MarketPlace

---
+++ client/src/components/FilterSideBar.jsx
@@ -0,0 +1,63 @@
+import { FilterIcon, X, XIcon } from "lucide-react";
+import React, { useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+const FilterSideBar = ({
+  showFilterPhone,
+  setShowFilterPhone,
+  filters,
+  setFilters,
+}) => {
+    const [searchParams ,setSearchParams] = useSearchParams()
+    const [search , setSearch] = useState(searchParams.get("search") || "");
+    const navigate = useNavigate();
+    const onChangeSearch =(e)=>{
+        if(e.target.value){
+            setSearchParams({search : e.target.value});
+            setSearch(e.target.value);
+        }else{
+            navigate('/marketplace');
+            setSearch("");
+        }
+    }
+  return (
+    <div
+      className={`${
+        showFilterPhone ? "max-sm:fixed" : "max-sm:hidden"
+      } max-sm:inset-0 z-100 max-sm:h-screen max-sm:overflow-scroll bg-white rounded-lg shadow-sm border border-gray-200 h-fit sticky top-24 md:min-w-[300px]`}
+    >
+      <div className="p-4 border-b border-gray-200">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-2 text-gray-700">
+            <FilterIcon className="size-4" />
+            <h3 className="font-semibold">Filters</h3>
+          </div>
+          <div className="flex items-center gap-2">
+            <X className="size-6 text-gray-500 hover:text-gray-700 p-1 hover:bg-gray-100 transition-color cursor-pointer" />
+            <button
+              onClick={() => setShowFilterPhone(false)}
+              className="sm:hidden text-sm border text-gray-700 px-3 py-1 rounded"
+            >
+              Apply
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-4 space-y-6 sm:max-h-[calc(100vh-200px)] overflow-y-scroll no-scrollbar">
+        {/* Search Bar  */}
+        <div className="flex items-center justify-between">
+          <input
+          onChange={onChangeSearch}
+            value={search}
+            type="text"
+            placeholder="Search by username, platform, niche, etc."
+            className="w-full text-sm px-3 py-2 border  border-gray-300 rounded-md outline-indigo-500"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FilterSideBar;
--- client/src/pages/MarketPlace.jsx
+++ client/src/pages/MarketPlace.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { ArrowLeftIcon, FilterIcon, Verified } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import {useSelector} from "react-redux"
+import ListingCards from "../components/ListingCards";
+import FilterSideBar from "../components/FilterSideBar";
 const MarketPlace = () => {
-  return (
-    <div>
-        <h1>MarketPlace</h1>
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const [showFilterPhone , setShowFilterPhone] = useState(false);
+  const [filters ,setFilters] = useState({
+    platform : null,
+    maxPrice : 100000,
+    minFollowers : 0,
+    niche : null,
+    verified : false,
+    monetized :  false 
+  })
 
-export default MarketPlace
+  const {listings} = useSelector(state=>state.listing);
+  const filteredListings = listings.filter((listing)=>{
+    return true;
+  })
+  return (
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32">
+      <div className="flex items-center justify-between text-slate-500">
+        <button
+          className="flex items-center gap-2 py-5"
+          onClick={() => {
+            navigate("/");
+            scrollTo(0, 0);
+          }}
+        >
+          {" "}
+          <ArrowLeftIcon className="size-4" /> Back to Home
+        </button>
+        <button className="flex sm:hidden items-center gap-2 py-5" onClick={()=>setShowFilterPhone(true)}>
+          {" "}
+          <FilterIcon className="size-4" /> Filters
+        </button>
+      </div>
+
+
+
+      <div className="relative flex items-start justify-between gap-8 pb-8">
+        {/* Filter  */}
+        
+          <FilterSideBar setFilters={setFilters} filters={filters} showFilterPhone={showFilterPhone} setShowFilterPhone={setShowFilterPhone}/>
+        
+
+        {/* Social Profile List  */}
+        <div className="flex-1 grid xl:grid-cols-2 gap-4">
+          {filteredListings.sort((a,b)=>a.featured ? -1 : b.featured ? 1 : 0).map((listing , index)=>(
+            <ListingCards key={index} listing={listing}/>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MarketPlace;
