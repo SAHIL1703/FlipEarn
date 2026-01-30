cffb5ec730083a107aa205d29d95a9c854f8f533
sahilpisal220 <sahilpisal1703@gmail.com>
2026-01-30 21:14:25 +0530

    Added the INNGEST to the project

--- server/server.js
+++ server/server.js
@@ -2,17 +2,24 @@
 import express from "express"
 import "dotenv/config";
 import cors from "cors";
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 
 //Adding the Middleware
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
+
 
 
 app.get("/" , (req,res)=>{
     res.send("Server is Live")
 })
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 //Adding PORT
 const PORT = process.env.PORT || 3000;
